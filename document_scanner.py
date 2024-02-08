8813e07c8a5d5b492b364dedd08717157e89a1a5
HannahGenerana <hannahgenerana@gmail.com>
2024-02-09 07:06:29 +0800

    pseudocode

---
+++ document_scanner.py
@@ -0,0 +1,15 @@
+# pseudocode
+
+# video set up
+
+# detect the paper to scan
+
+# scan wrapping
+
+# convert image to black and white
+
+# create an optical characted recognition
+
+# save the image and text to be printed
+
+#https://www.youtube.com/watch?v=W3DzSm8WI1g - yt link of the code 
