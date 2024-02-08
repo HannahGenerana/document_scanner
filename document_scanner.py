6231f1eebbf3b9988ab87121d6f11c18f8e18086
HannahGenerana <hannahgenerana@gmail.com>
2024-02-09 07:10:01 +0800

    set up of video

--- document_scanner.py
+++ document_scanner.py
@@ -1,6 +1,14 @@
 # pseudocode
+import cv2
+import numpy as np 
+from imutils.perspective import four_point_transform
+import pytesseract
 
 # video set up
+cap = cv2.VideoCapture (0 + cv2.CAP_DSHOW)
+width, height = 500, 450
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
 # detect the paper to scan
 
